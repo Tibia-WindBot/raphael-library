934d91810ba823f4dcf9dc8ab43838ed0af6c164
Raphael <r.mobis@gmail.com>
2014-01-24 20:34:33 -0200

    Add rendering time output to build script God, it sucks to have no internet; I have to go after these little shitty things to do

--- build.lua
+++ build.lua
@@ -6,4 +6,4 @@ local handler = io.open(config.files.dest, 'w+')
 handler:write(TLua.render(config.files.layout, config))
 handler:close()
 
-print(config.name .. ' v' .. config.version .. ' succesfully built!')
+print(config.name .. ' v' .. config.version .. ' succesfully built! (' .. os.clock() * 1000 .. 'ms)')
