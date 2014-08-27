7b578a7a6401946e0fbb002a429216eef4c1ef4d
rmobis <r.mobis@gmail.com>
2014-08-27 00:52:49 -0300

    Add a shitload of stuff to Area Class

--- src/Area Class.lua
+++ src/Area Class.lua
@@ -2,6 +2,11 @@ Area = { __class = 'Area' }
 AreaMT = { __index = Area }
 
 function Area:new(firstCorner, width, height)
+	-- Special handling for rect
+	if type(firstCorner) == 'userdata' and firstCorner.objtype == 'rect' then
+		return Area:new(Point:new(firstCorner.left, firstCorner.top), firstCorner.width, firstCorner.height)
+	end
+
 	firstCorner = Point:new(firstCorner)
 
 	local secondCorner = Point:new(width)
@@ -21,6 +26,56 @@ function Area:new(firstCorner, width, height)
 	return newObj
 end
 
+function Area:createFromWaypoint(waypoint)
+	local topLeft = Point:new(get(waypoint, 'Coordinates'):match(REGEX_COORDS))
+	local width, height = get(waypoint, 'Range'):match(REGEX_RANGE)
+
+	return Area:new(topLeft, width, height)
+end
+
+function Area:createFromSpecialArea(specialArea)
+	local topLeft = Point:new(get(specialArea, 'Coordinates'):match(REGEX_COORDS))
+	local width, height = get(specialArea, 'Size'):match(REGEX_RANGE)
+
+	return Area:new(topLeft, width, height)
+end
+
+function Area:extend(top, right, bottom, left)
+	-- This gives us a CSS-like workflow; the one that usually works in margin
+	-- and padding shorthands
+	top    = top    or 0
+	right  = right  or top
+	bottom = bottom or top
+	left   = left   or right
+
+	self.topLeft = self.topLeft - Point:new(top, left)
+	self.botRight = self.botRight + Point:new(bottom, right)
+end
+
+function Area:getLeft()
+	return self.topLeft.x
+end
+
+function Area:getTop()
+	return self.topLeft.y
+end
+
+function Area:getRight()
+	return self.botRight.x
+end
+
+function Area:getBottom()
+	return self.botRight.y
+end
+
+function Area:getWidth()
+	return self.botRight.x - self.topLeft.x
+end
+
+function Area:getHeight()
+	return self.botRight.y - self.topLeft.y
+end
+
 function Area:hasPoint(point, y)
 	point = Point:new(point, y)
 
@@ -28,6 +83,13 @@ function Area:hasPoint(point, y)
 	       point.y >= self.topLeft.y  and
 	       point.x <= self.botRight.x and
 	       point.y <= self.botRight.y
+end
+
+function Area:move(point, y)
+	point = Point:new(point, y)
+
+	self.topLeft = self.topLeft + point
+	self.botRight = self.botRight + point
 end
 
 function AreaMT:__tostring()
